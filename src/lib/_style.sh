010b5a69cda258a44383c7fa4861654b56567999
AMJones <am@jonesiscoding.com>
2026-08-31 12:59:03 +0000

    Don't initialize more than once

--- src/lib/_style.sh
+++ src/lib/_style.sh
@@ -5,22 +5,24 @@ SPINNER_PID=""
 ## region ############################################## Variable Declarations
 
 # shellcheck disable=SC2034
-if [[ -n "$NO_COLOR" || "$TERM" =~ ^(dumb|emacs)$ || -n "$CI" ]] || ! command -v tput > /dev/null 2>&1; then
-  RED="" && GREEN="" && YELLOW="" && BLUE="" && MAGENTA="" && CYAN="" && WHITE="" && RESET=""
-else
-  RED=$(tput setaf 1) \
-    && GREEN=$(tput setaf 2) \
-    && YELLOW=$(tput setaf 3) \
-    && BLUE=$(tput setaf 4) \
-    && MAGENTA=$(tput setaf 5) \
-    && CYAN=$(tput setaf 6) \
-    && WHITE=$(tput setaf 7) \
-    && RESET=$(tput sgr0)
+if [[ -z "$HR" ]]; then
+  if [[ -n "$NO_COLOR" || "$TERM" =~ ^(dumb|emacs)$ || -n "$CI" ]] || ! command -v tput > /dev/null 2>&1; then
+    RED="" && GREEN="" && YELLOW="" && BLUE="" && MAGENTA="" && CYAN="" && WHITE="" && RESET=""
+  else
+    RED=$(tput setaf 1) \
+      && GREEN=$(tput setaf 2) \
+      && YELLOW=$(tput setaf 3) \
+      && BLUE=$(tput setaf 4) \
+      && MAGENTA=$(tput setaf 5) \
+      && CYAN=$(tput setaf 6) \
+      && WHITE=$(tput setaf 7) \
+      && RESET=$(tput sgr0)
+  fi
+  CHECK="${GREEN}✓${RESET}"
+  CROSS="${RED}✗${RESET}"
+  FRAMES=('⠋' '⠙' '⠹' '⠸' '⠼' '⠴' '⠦' '⠧' '⠇' '⠏')
+  HR="---------------------------------------------"
 fi
-CHECK="${GREEN}✓${RESET}"
-CROSS="${RED}✗${RESET}"
-FRAMES=('⠋' '⠙' '⠹' '⠸' '⠼' '⠴' '⠦' '⠧' '⠇' '⠏')
-HR="---------------------------------------------"
 
 # shellcheck disable=SC2034
 declare -r RED GREEN YELLOW BLUE MAGENTA CYAN WHITE RESET CHECK CROSS FRAMES HR
